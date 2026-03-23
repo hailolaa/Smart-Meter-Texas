4e1b0a4343288ffd282071d2274af24287aacdfd
Hailemichael Molla <167626795+hailolaa@users.noreply.github.com>
2026-03-23 23:27:23 +0300

    add mock repository

---
+++ lib/features/energy/data/repositories/mock_energy_repository.dart
@@ -0,0 +1,22 @@
+import '../../domain/entities/energy_summary.dart';
+import '../../domain/repositories/energy_repository.dart';
+
+class MockEnergyRepository implements EnergyRepository {
+  @override
+  Future<EnergySummary> getEnergySummary() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+
+    return EnergySummary(
+      currentSpend: 4.23,
+      totalBudget: 8.00,
+      usedPercentage: 0.53,
+      percentVsYesterday: -0.12,
+      remainingAmount: 3.77,
+      airConditionerCost: 2.15,
+      kwhToday: 12.5,
+      kwhTrend: 0.08,
+      centsPerKwh: 15.5,
+      centsTrend: -0.03,
+    );
+  }
+}
